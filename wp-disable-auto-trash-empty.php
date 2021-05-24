dc7aedf0ba7a90ff2e5f5c8510fa53650ce2bda4
Ross Alexander <rossaalexander@gmail.com>
2021-05-24 10:35:16 +0200

    Initial commit

---
+++ wp-disable-auto-trash-empty.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: Ralex Disable WordPress Auto Trash Empty
+Plugin URI: https://github.com/disable-wordpress-auto-trash
+Description: Prevent WordPress from automatically empty trash every 30 days.
+Version: 1.0
+Author: rralex
+Author URI: https://github.com/rossalexander
+License: GPL2
+*/
+
+add_action('init', 'viabold_disable_schedule_delete');
+function viabold_disable_schedule_delete()
+{
+    remove_action('wp_scheduled_delete', 'wp_scheduled_delete');
+}
+
